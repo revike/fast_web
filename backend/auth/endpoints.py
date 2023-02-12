08f8e11b99799e4ce16733a0ffe62147914bb528
revike <revike@ya.ru>
2023-02-12 14:33:39 +0300

    delete user

--- backend/auth/endpoints.py
+++ backend/auth/endpoints.py
@@ -1,19 +1,21 @@
-from typing import List
+from typing import List, Dict
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
-from backend.auth.schemas import UserRead, UserCreate, UserCreateResponse
+from backend.auth.schemas import UserRead, UserCreate, UserCreateResponse, \
+    UserList, UserDelete
 from backend.auth.session import UserSession
 from backend.core.session import get_async_session
 
 user_router = APIRouter()
 
 
-@user_router.get('/list', response_model=List)
+@user_router.get('/list', response_model=List[UserList])
 async def get_users(
-        session: AsyncSession = Depends(get_async_session)) -> List:
+        session: AsyncSession = Depends(
+            get_async_session)) -> List[Dict[str, UserList]]:
     users = await UserSession(session).get_users()
     return users
 
@@ -37,3 +39,14 @@ async def create_user(
         session: AsyncSession = Depends(
             get_async_session)) -> UserCreateResponse:
     return await UserSession(session).create_user(body)
+
+
+@user_router.delete('/delete/{user_id}', response_model=UserDelete)
+async def delete_user(
+        user_id: int,
+        session: AsyncSession = Depends(get_async_session)) -> UserDelete:
+    delete_user_id = await UserSession(session).delete_user(user_id)
+    if delete_user_id is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'User with id {user_id} not found')
+    return UserDelete(deleted_user_id=delete_user_id)
